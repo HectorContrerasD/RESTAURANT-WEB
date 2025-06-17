5219e888edd320dab415494091ba80e186e38d7b
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2025-06-17 01:18:39 -0600

    Funcionalidad terminada

---
+++ Restaurant.Web/Pages/AgregarProducto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Restaurant.Web.Pages
+{
+    public class AgregatProductoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
